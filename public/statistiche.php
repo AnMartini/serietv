d1b462a287d8b1631f4a8c0efc6dfe77d36aedd6
Andrea Martini <andrea.martini@me.com>
2016-05-26 17:03:51 +0200

    update colora() function

--- public/statistiche.php
+++ public/statistiche.php
@@ -3,13 +3,15 @@ include_once("pdo.php");
 error_reporting(E_ALL);
 
 function colora($numero) {
-	$red = ($numero * 95) % 255;
-	$green = ($numero * 23) % 255;
-	$blue = ($numero * 130) % 255;
+	$red = ($numero * 16) % 256;
+	$green = ($numero * 127) % 256;
+	$blue = ($numero * 240) % 256;
+	if ($red == 0 && $blue == 0) {
+		$green += 100;
+	}
 	$color = "rgb($red, $green, $blue)";
 	return $color;
 }
-
 $mesi = [ 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0 ];
 $giorni = [ 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0 ];
 $ore = [ 0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0, 13 => 0, 14 => 0, 15 => 0, 16 => 0, 17 => 0, 18 => 0, 19 => 0, 20 => 0, 21 => 0, 22 => 0, 23 => 0 ];
